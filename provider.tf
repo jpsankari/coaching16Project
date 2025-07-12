acf6e35a2055378e7bf80912333a45f2a960ea5c
jpsankari <jpsankari@hotmail.com>
2025-07-12 21:55:42 +0800

    updated provider.tf

--- provider.tf
+++ provider.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 terraform {
-    required_version = ">= 1.12.2"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_version = ">= 1.12.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
