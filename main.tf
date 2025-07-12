618afc62de2f40c7564a6bb35e95898911d84cf6
jpsankari <jpsankari@hotmail.com>
2025-07-12 23:12:36 +0800

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "aws_dynamodb_table" "c10-sankari-short_ids" {
-  name         = "c10_short-ids"
+resource "aws_dynamodb_table" "c10_short_ids" {
+  name         = "c10_short_ids"
   billing_mode = "PAY_PER_REQUEST"  # auto-scaling, no need to set read/write capacity
-  hash_key = "c10_short_id"  # primary key
+  hash_key = "c10_short_ids"  # primary key
   attribute {
-    name = "c10_sankari_short_id"
+    name = "c10_short_ids"
     type = "S" # string
   }
   # Optional: TTL to expire short links (e.g., after 30 days)
@@ -12,7 +12,7 @@ resource "aws_dynamodb_table" "c10-sankari-short_ids" {
     enabled        = true
   }
   tags = {
-    Name        = "c10_sankari_short-ids"
+    Name        = "c10_Sankari_short_ids"
     Environment = "dev"
   }
 }
