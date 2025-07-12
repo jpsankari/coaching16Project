595a670fb474be3fe503452f25c3656cd0f35168
jpsankari <JPSANKARI@HOTMAIL.COM>
2025-07-12 22:45:51 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,16 @@
 resource "aws_dynamodb_table" "short_ids" {
   name         = "short-ids"
   billing_mode = "PAY_PER_REQUEST"  # auto-scaling, no need to set read/write capacity
-
   hash_key = "short_id"  # primary key
-
   attribute {
     name = "short_id"
     type = "S"  # string
   }
-
   # Optional: TTL to expire short links (e.g., after 30 days)
   ttl {
     attribute_name = "expire_at"
     enabled        = true
   }
-
   tags = {
     Name        = "short-ids"
     Environment = "dev"
