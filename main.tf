b79e06b7ed7b7efabf933249f79b36d49ccf9794
jpsankari <jpsankari@hotmail.com>
2025-07-12 22:51:29 +0800

    updload

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "c10-sankari-short_ids" {
   name         = "short-ids"
-  billing_mode = "PAY_PER_REQUEST"  # auto-scaling, no need to set read/write capacity
-  hash_key = "short_id"  # primary key
+  billing_mode = "PAY_PER_REQUEST" # auto-scaling, no need to set read/write capacity
+  hash_key     = "short_id"        # primary key
   attribute {
     name = "short_id"
     type = "S" # string
